d9e5a4c98e09b518d762b51336fa257d47555113
yangkejian <yangkejian259@hfjy.com>
2019-02-20 15:06:32 +0800

    save

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangkejian
+ * Date: 2019-2-20
+ * Time: 15:00
+ */
+require_once './vendor/autoload.php';
+
+use ttgoat\egg\Tools;
+
+Tools::Hello();
---
+++ src/ttgoat/egg/Tools.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangkejian
+ * Date: 2019-2-20
+ * Time: 14:31
+ */
+
+namespace ttgoat\egg;
+
+
+class Tools
+{
+    public function __construct(){}
+
+    public static function Hello()
+    {
+        echo 'Hello world';
+    }
+}
